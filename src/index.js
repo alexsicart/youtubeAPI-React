bf4fdbadfd4b59e32d2ceedf26879d8bebc9f2cc
alexsicart <alex@sharge.io>
2017-09-01 11:04:23 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const API_KEY = 'AIzaSyCkAHOTWopyQ7J60vmSiET320NO7rWOqwc';
+
+const App = () => {
+  return <div> Hi! </div>;
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
