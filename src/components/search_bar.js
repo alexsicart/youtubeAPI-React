1c5108904cfd8b0409c1f6f924f7a81f2e9d74cf
alexsicart <alex@sharge.io>
2017-09-01 12:00:55 +0200

    Input logic finished

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -6,10 +6,14 @@ class SearchBar extends Component {
 
     this.state = { term: '' };
   }
-  
+
   render() {
     return(
-      <input onChange={(event) => console.log(event.target.value)} />
+      <div>
+        <input
+          value={this.state.term}
+          onChange={(event) => this.setState({ term: event.target.value })} />
+      </div>
     )
   }
 
